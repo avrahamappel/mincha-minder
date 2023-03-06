cd90d5375d641111bea367914745686aa88c4632
Avraham Appel <aappel248@gmail.com>
2023-03-06 01:12:55 -0500

    Try to make rust-analyzer stop vrooming

--- flake.nix
+++ flake.nix
@@ -2,23 +2,27 @@
   description = "Mincha Minder: generate a calendar of your mincha events";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, rust-overlay, ... }:
-    let
-      inherit (rust-overlay.inputs) nixpkgs flake-utils;
-    in
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
+
         pkgs = import nixpkgs { inherit system overlays; };
+
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.rust-analyzer
-            pkgs.rust-bin.stable.latest.default
+            rust
           ];
         };
       });
