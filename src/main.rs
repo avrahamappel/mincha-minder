3e19629edc8f62240dd36d93a1f16f2dd4993879
Avraham Appel <aappel248@gmail.com>
2023-10-31 21:35:58 -0400

    No server, just generate the calendar

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use std::net::SocketAddr;
-use std::str::FromStr;
-
 use icalendar::Calendar;
-use warp::Filter;
 
 // mod caldav;
 mod mincha_minder;
@@ -14,25 +10,7 @@ fn schedule() -> Calendar {
     Calendar::from(sch)
 }
 
-#[tokio::main]
-async fn main() {
-    // Cheap debugging
-    let log = warp::log::custom(|info| {
-        eprintln!("{} {} {}", info.method(), info.path(), info.status(),);
-    });
-
-    warp::serve(
-        warp::path("caldav")
-            .and(warp::filters::any::any())
-            .and(warp::filters::path::full())
-            .and(warp::filters::method::method())
-            .map(|path, method| {
-                println!("PATH: {path:?}, METHOD: {method}");
-
-                schedule().to_string()
-            })
-            .with(log),
-    )
-    .run(SocketAddr::from_str("127.0.0.1:8000").unwrap())
-    .await
+fn main() {
+    let sch = schedule();
+    println!("{sch}");
 }
